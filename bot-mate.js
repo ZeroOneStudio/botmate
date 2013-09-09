d78e4fbb9241d49c8c7659d58a56002d0971257a
Michael Romanenko <michael@romanenko.kg>
2013-09-10 00:16:56 +0600

    DRYed logTimestamp

--- bot-mate.js
+++ bot-mate.js
@@ -6,6 +6,11 @@ var phantom = require('phantom'),
     colors = require('colors'),
     moment = require('moment');
 
+function logTimestamp() {
+    var timestamp = moment().format('D MMM YYYY, H:m');
+    return timestamp.blue;
+}
+
 phantom.create(function (ph) {
     http.createServer(function (req, res) {
         var query, targetUrl, reqMoment;
@@ -13,19 +18,19 @@ phantom.create(function (ph) {
         if (typeof query === 'object' && query.host !== undefined && query.fragment !== undefined) {
             targetUrl = "http://" + query.host + "/#!" + query.fragment;
             reqMoment = moment();
-            console.log(moment().format('D MMM YYYY, H:m').blue, 'Processing url:'.yellow, targetUrl, 'for ' + req.headers['user-agent'].grey + ' (' + req.headers.host + ')');
+            console.log(logTimestamp(), 'Processing url:'.yellow, targetUrl, 'for ' + req.headers['user-agent'].grey + ' (' + req.headers.host + ')');
             ph.createPage(function (page) {
                 page.set('onLoadFinished', function (status) {
                     if (status !== 'success') {
                         res.writeHead(500, {'Content-Type': 'text/plain'});
                         res.end("Couldn't open desired page\n", 'utf-8');
-                        console.log(moment().format('D MMM YYYY, H:m').blue, 'Error occured with url:'.red, targetUrl, '(' + req.headers.host + ')');
+                        console.log(logTimestamp(), 'Error occured with url:'.red, targetUrl, '(' + req.headers.host + ')');
                         page.close();
                     } else {
                         page.get('content', function (content) {
                             res.writeHead(200, {'Content-Type': 'text/html'});
                             res.end(content, 'utf-8');
-                            console.log(moment().format('D MMM YYYY, H:m').blue, 'Served static version of:'.green, targetUrl, '(' + req.headers.host + ')', 'in', moment().diff(reqMoment, 'milliseconds') + 'ms');
+                            console.log(logTimestamp(), 'Served static version of:'.green, targetUrl, '(' + req.headers.host + ')', 'in', moment().diff(reqMoment, 'milliseconds') + 'ms');
                             page.close();
                         });
                     }
@@ -35,7 +40,7 @@ phantom.create(function (ph) {
         } else {
             res.writeHead(400, {'Content-Type': 'text/plain'});
             res.end("Something is wrong with request arguments. Please read documentation of Botmate.\n", 'utf-8');
-            console.log(moment().format('D MMM YYYY, H:m').blue, 'Wrong request arguments'.red, 'from ' + req.headers['user-agent'].grey + ' (' + req.headers.host + ')');
+            console.log(logTimestamp(), 'Wrong request arguments'.red, 'from ' + req.headers['user-agent'].grey + ' (' + req.headers.host + ')');
         }
     }).listen(3001, '127.0.0.1');
 });
