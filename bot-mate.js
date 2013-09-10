d1fad7e3703d38e08f2b9ee382dc93ee36675370
Michael Romanenko <michael@romanenko.kg>
2013-09-10 06:00:54 +0600

    Got rid of else statements

--- bot-mate.js
+++ bot-mate.js
@@ -8,7 +8,7 @@ var phantom = require('phantom'),
 
 var Logger = {
     logTimestamp: function () {
-        var timestamp = moment().format('D MMM YYYY, H:mm');
+        var timestamp = moment().format('D MMM YYYY, H:mm:ss');
         return timestamp.blue;
     },
     compose: function (action, targetUrl, req, reqMoment) {
@@ -28,6 +28,7 @@ var Logger = {
         record.push(this.logTimestamp());
         record = record.concat(this.compose.apply(this, arguments));
         console.log.apply(console, record);
+        return record.join('');
     }
 };
 
@@ -36,32 +37,30 @@ phantom.create(function (ph) {
         var query, targetUrl,
             reqMoment = moment();
         query = url.parse(req.url, true).query;
-        if (typeof query === 'object' && query.host !== undefined && query.fragment !== undefined) {
-            targetUrl = "http://" + query.host + "/#!" + query.fragment;
-            Logger.perform('processing', targetUrl, req);
-            ph.createPage(function (page) {
-                page.set('onLoadFinished', function (status) {
-                    if (status !== 'success') {
-                        res.writeHead(500, {'Content-Type': 'text/plain'});
-                        res.end("Couldn't open desired page\n", 'utf-8');
-                        Logger.perform('request-error', targetUrl, req);
-                        page.close();
-                    } else {
-                        page.get('content', function (content) {
-                            res.writeHead(200, {'Content-Type': 'text/html'});
-                            res.end(content, 'utf-8');
-                            Logger.perform('success', targetUrl, req, reqMoment);
-                            page.close();
-                        });
-                    }
-                });
-                page.open(targetUrl);
-            });
-        } else {
+        if (query.host === undefined && query.fragment === undefined) {
             res.writeHead(400, {'Content-Type': 'text/plain'});
             res.end("Something is wrong with request arguments. Please read documentation of Botmate.\n", 'utf-8');
-            Logger.perform('wrong-args', targetUrl, req);
+            return Logger.perform('wrong-args', undefined, req);
         }
+        targetUrl = "http://" + query.host + "/#!" + query.fragment;
+        Logger.perform('processing', targetUrl, req);
+        ph.createPage(function (page) {
+            page.set('onLoadFinished', function (status) {
+                if (status !== 'success') {
+                    res.writeHead(500, {'Content-Type': 'text/plain'});
+                    res.end("Couldn't open desired page\n", 'utf-8');
+                    Logger.perform('request-error', targetUrl, req);
+                    return page.close();
+                }
+                page.get('content', function (content) {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end(content, 'utf-8');
+                    Logger.perform('success', targetUrl, req, reqMoment);
+                    return page.close();
+                });
+            });
+            page.open(targetUrl);
+        });
     }).listen(3001, '127.0.0.1');
 });
 
